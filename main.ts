4c4786d85bbde037cd5ef2f90519e7676621e557
Tony081113 <141484094+Tony081113@users.noreply.github.com>
2024-03-03 07:45:53 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    you.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.AB, function () {
+    you.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    you.change(LedSpriteProperty.X, 1)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    you.change(LedSpriteProperty.Y, -1)
+})
+let you: game.LedSprite = null
+game.setScore(0)
+you = game.createSprite(2, 2)
+let sb = game.createSprite(randint(0, 5), randint(0, 5))
+basic.pause(100000)
+game.gameOver()
+basic.showNumber(game.score())
+basic.forever(function () {
+    if (you.isTouching(sb)) {
+        game.addScore(1)
+    }
+    you.set(LedSpriteProperty.X, 0)
+    you.set(LedSpriteProperty.Y, 0)
+    sb.set(LedSpriteProperty.X, randint(0, 5))
+    sb.set(LedSpriteProperty.Y, randint(0, 5))
+})
